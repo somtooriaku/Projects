bcdd4120410a92abdbfcfc549370abb5b9142f7a
somtooriaku <59647845+somtooriaku@users.noreply.github.com>
2023-03-24 01:57:16 -0400

    1

---
+++ Java/basic_calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+  public Calculator(){
+  }
+  public int add(int a, int b){
+    int result = a + b;
+    return result;
+  }
+  public int subtract(int a, int b){
+    int result = a - b;
+    return result;
+  }
+  public int multipy(int a, int b){
+    int result = a * b;
+    return result;
+  }
+  public double divide(int a, int b){
+    int result = a/b;
+    return result;
+  }
+  public int modulo(int a, int b){
+    int result = a % b;
+    return result;
+  }
+  public static void main(String[] args){  
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+    System.out.println(myCalculator.subtract(45, 11));
+  }
+}
